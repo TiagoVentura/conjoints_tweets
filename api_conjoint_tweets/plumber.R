9076b1cc3ea4a4481d2ca2809460ddc5309c0302
Tiago Ventura <venturat@umd.edu>
2023-07-31 10:07:41 -0400

    api new code

--- api_conjoint_tweets/plumber.R
+++ api_conjoint_tweets/plumber.R
@@ -8,10 +8,21 @@ function(msg=""){
 }
 
 #* Endpoint that bypasses serialization
+#* @param issue_query
 #* @get /random_tweets
-function(){
-  outputs <- read.csv("list_images.csv") # upload all the image
+function(issue_query=""){
+  outputs <- read.csv("list_images.csv")  %>% 
+              dplyr::filter(p_issue==issue_query) %>%
+              dplyr::select(file_name=p_output) 
   image <- outputs$file_name[sample(1:nrow(outputs), 1)] # randomize
-  url <- paste0("https://raw.githubusercontent.com/TiagoVentura/conjoints_tweets/main/output/output_api/", image) # get the image
-  list(url=url) # output
+  url <- paste0("https://raw.githubusercontent.com/TiagoVentura/conjoints_tweets/main/output/out_testing_real_tw/", image) # get the image
+  # capture output
+  out = list(url=url) # output
+  
+  # change name
+  name <- paste0("url.", issue_query)
+  names(out) = name
+  # return
+  out
 }
+
--- api_conjoint_tweets/run_api.R
+++ api_conjoint_tweets/run_api.R
@@ -2,4 +2,3 @@ library(plumber)
 options(httr_oob_default = TRUE)
 root <- pr("/home/rstudio/plumber_api/plumber_api.r")
 pr_run(root, host = "0.0.0.0", port = 8000)
-y
